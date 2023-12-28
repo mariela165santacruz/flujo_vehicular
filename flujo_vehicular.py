a759408d992d947a7cdb6e4284e9acf0dfab397e
Mariela Santa Cruz <mariela165santacruz@gmail.com>
2023-12-27 22:36:28 -0300

    Arreglo en dataset

--- flujo_vehicular.py
+++ flujo_vehicular.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('dataset_flujo_vehicular.csv')
+df = pd.read_csv('dataset_flujo_vehicular.csv', sep=';')
 
 #Exploración de Datos
 print(df.head()) #muestra las primeras lineas del DataFrame
@@ -17,6 +17,6 @@ plt.title('Distribución de CANTIDAD')
 plt.show()
 
 # Diagrama de dispersión entre dos variables
-sns.scatterplot(x='CANTIDAD', y='SENTIDO', data=df)
-plt.title('Diagrama de Dispersión entre Columna1 y Columna2')
+sns.scatterplot(x='CANTIDAD', y='BARRIO', data=df)
+plt.title('Diagrama de Dispersión entre Cantidad y Sentido')
 plt.show()
