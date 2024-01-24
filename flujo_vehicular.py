31d6e672d8c28d898a9e8d1e364e8fc337e65bd1
Mariela Santa Cruz <mariela165santacruz@gmail.com>
2024-01-24 18:47:48 -0300

    Se agrega mapa de calor en formato html

--- flujo_vehicular.py
+++ flujo_vehicular.py
@@ -1,22 +1,30 @@
-#Importación de bibliotecas utilizadas
+# Importación de bibliotecas utilizadas
 import pandas as pd
+import geopandas as gpd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import folium
+from folium.plugins import HeatMap
+from shapely.geometry import Point
 
+# Cargar datos desde un archivo CSV
 df = pd.read_csv('dataset_flujo_vehicular.csv', sep=';')
 
-#Exploración de Datos
-print(df.head()) #muestra las primeras lineas del DataFrame
+# Filtrar filas con coordenadas válidas
+df = df.dropna(subset=['LONGITUD', 'LATITUD'])
 
-#Estadisticas descriptivas
-print(df.describe())
+# Crear GeoDataFrame a partir de DataFrame
+geometry = [Point(xy) for xy in zip(df['LONGITUD'], df['LATITUD'])]
+gdf = gpd.GeoDataFrame(df, geometry=geometry)
+gdf.crs = "EPSG:4326"  # Asignar el sistema de referencia espacial (CRS)
 
-# Visualización de distribución de variable
-sns.histplot(df['CANTIDAD'], bins=20, kde=True)
-plt.title('Distribución de CANTIDAD')
-plt.show()
+# Crear un mapa centrado en CABA
+caba_map = folium.Map(location=[-34.61, -58.38], zoom_start=12)
 
-# Diagrama de dispersión entre dos variables
-sns.scatterplot(x='CANTIDAD', y='BARRIO', data=df)
-plt.title('Diagrama de Dispersión entre Cantidad y Sentido')
-plt.show()
+# Crear un mapa de calor con las coordenadas
+heat_data = [[point.xy[1][0], point.xy[0][0]] for point in gdf.geometry]
+HeatMap(heat_data).add_to(caba_map)
+
+# Guardar el mapa como un archivo HTML
+caba_map.save("mapa_calor_caba.html")
+
